1eda92094039d8ffd7ff785ba26cbc0dfb36b1ca
Vishnunadh V B <vishnunadh25@gmail.com>
2025-04-03 12:46:24 +0530

    Update menu-script.js

--- menu-script.js
+++ menu-script.js
@@ -177,7 +177,7 @@ document.getElementById('addToReservation').addEventListener('click', () => {
         localStorage.setItem('spiceHavenOrder', JSON.stringify(orderData));
         
         // Redirect to reservation page
-        window.location.href = 'index.html';
+        window.location.href = 'menu2.html';
     } else {
         alert('Please select at least one item to add to your reservation.');
     }
